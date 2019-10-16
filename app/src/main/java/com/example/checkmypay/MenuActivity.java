8d17fa323cedd069ede2a2d31b7a1ddbd9b9ab38
Mavgi <avgimatan@gmail.com>
2019-10-16 17:35:57 +0300

    Add menu activities

--- app/src/main/java/com/example/checkmypay/MenuActivity.java
+++ app/src/main/java/com/example/checkmypay/MenuActivity.java
@@ -2,6 +2,7 @@ package com.example.checkmypay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MenuActivity extends AppCompatActivity {
@@ -12,6 +13,41 @@ public class MenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_menu);
 
 
+    }
 
+    // check this function
+    public void goToActivity(Class<?> className) {
+        Intent intent = new Intent(this, className);
+        intent.putExtra("user", user);
+        startActivity(intent);
+        finish();
+    }// check
+
+    public void goToSetting() {
+        Intent intent = new Intent(this, SettingActivity.class);
+        intent.putExtra("user", user);
+        startActivity(intent);
+        finish();
+    }
+
+    public void goToRate() {
+        Intent intent = new Intent(this, RateActivity.class);
+        intent.putExtra("user", user);
+        startActivity(intent);
+        finish();
+    }
+
+    public void goToPaycheck() {
+        Intent intent = new Intent(this, PaycheckActivity.class);
+        intent.putExtra("user", user);
+        startActivity(intent);
+        finish();
+    }
+
+    public void goToSalary() {
+        Intent intent = new Intent(this, SalaryActivity.class);
+        intent.putExtra("user", user);
+        startActivity(intent);
+        finish();
     }
 }
---
+++ app/src/main/java/com/example/checkmypay/RateActivity.java
@@ -0,0 +1,15 @@
+package com.example.checkmypay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rate);
+    }
+
+}
