15eba1c0153b05cf4f6194e0143c411c164c8584
Mavgi <avgimatan@gmail.com>
2019-10-28 22:36:05 +0200

    update grid layout in menu

--- app/src/main/java/com/example/checkmypay/MenuActivity.java
+++ app/src/main/java/com/example/checkmypay/MenuActivity.java
@@ -4,15 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MenuActivity extends AppCompatActivity {
 
     private User user;
+    private Map<String,Button> buttons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,26 +26,32 @@ public class MenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_menu);
 
         user = (User) getIntent().getSerializableExtra("user");
+        buttons = new HashMap<>();
 
         //create Grid
-        LinearLayout main_layout = findViewById(R.id.main_layout);
+        LinearLayout mainLayout = findViewById(R.id.main_layout);
         GridLayout gridLayout = createGridLayout(2, 2);
-        main_layout.addView(gridLayout);
+        mainLayout.addView(gridLayout);
 
-        for (int i = 0; i < 4; i++ ) {
-            Button button = new Button(this);
-            button.setBackgroundResource(R.drawable.common_google_signin_btn_text_dark);
+        buttons.put("My Paycheck",new Button(this));
+        buttons.put("My Salary",new Button(this));
+        buttons.put("My Rate",new Button(this));
+        buttons.put("My Setting",new Button(this));
+
+        buttons.get("My Paycheck").setText("My Paycheck");
+        buttons.get("My Salary").setText("My Salary");
+        buttons.get("My Rate").setText("My Rate");
+        buttons.get("My Setting").setText("My Setting");
+
+
+        for (Button button : buttons.values())
+        {
             button.setLayoutParams(new ViewGroup.LayoutParams(360,400));
-            button.setPadding(10,10,10,10);
-            button.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-                }
-            });
-
+            //button.setPadding(500,500,500,500);
+            button.setAllCaps(false);
             gridLayout.addView(button);
         }
+
 
 
     }
@@ -51,6 +63,14 @@ public class MenuActivity extends AppCompatActivity {
         gridLayout.setColumnCount(colsNum);
         gridLayout.setRowCount(rowsNum);
         gridLayout.setOrientation(GridLayout.HORIZONTAL);
+
+/*        RelativeLayout.LayoutParams layoutParams =
+                new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT);
+        RelativeLayout parentRelativeLayout = new RelativeLayout(this);
+        parentRelativeLayout.setLayoutParams(layoutParams);
+        parentRelativeLayout.setGravity(Gravity.CENTER);
+        parentRelativeLayout.addView(gridLayout);*/
 
         return gridLayout;
     }
