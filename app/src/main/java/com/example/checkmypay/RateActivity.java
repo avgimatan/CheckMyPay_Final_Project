acbc35d15c147add436ba59d5b0be46a94628cbd
tavbeza <49399501+tavbeza@users.noreply.github.com>
2019-11-02 17:46:21 +0200

    more of RateActivity

--- app/src/main/java/com/example/checkmypay/RateActivity.java
+++ app/src/main/java/com/example/checkmypay/RateActivity.java
@@ -1,8 +1,12 @@
 package com.example.checkmypay;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
+import android.graphics.Typeface;
+import android.graphics.fonts.Font;
 import android.os.Bundle;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -10,21 +14,28 @@ import android.widget.TextView;
 public class RateActivity extends AppCompatActivity {
 
     private LinearLayout linearLayout;
-    private TextView txt_title, txt_hourlyWage, txt_salaryStartDate, txt_salaryEndDate, txt_overtime, txt_creditPoints,
-                        txt_fromHour, txt_toHour, txt_providentFund /* kupat gemel */; // need more
 
-    private EditText input_hourlyWage, input_salaryStartDate, input_salaryEndDate, input_overtime, input_creditPoints,
-            input_fromHour, input_toHour, input_providentFund;
+    private EditText input_wage, input_startDate, input_endDate, input_overtime, input_credits,
+            input_fromHour, input_toHour, input_providentFund, input_advancedStudyFund;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rate);
 
-        linearLayout = findViewById(R.id.rate_linearLayout);
+        input_wage = findViewById(R.id.rate_input_hourly_wage);
+        input_startDate = findViewById(R.id.rate_input_start_date);
+        input_endDate = findViewById(R.id.rate_input_end_date);
+        input_overtime = findViewById(R.id.rate_input_overtime);
+        input_credits = findViewById(R.id.rate_input_credits);
+        input_fromHour = findViewById(R.id.rate_input_from_hour);
+        input_toHour = findViewById(R.id.rate_input_to_hour);
+        input_providentFund = findViewById(R.id.rate_input_provident_funds);
+        input_advancedStudyFund = findViewById(R.id.rate_input_advanced_study_fund);
 
-        //linearLayout.add
+
 
 
     }
+
 }
--- app/src/main/java/com/example/checkmypay/User.java
+++ app/src/main/java/com/example/checkmypay/User.java
@@ -1,10 +1,16 @@
 package com.example.checkmypay;
 
+import com.google.type.Date;
+
 import java.io.Serializable;
 
+// input_wage, input_startDate, input_endDate, input_overtime, input_credits,
+//            input_fromHour, input_toHour, input_providentFund, input_advancedStudyFund;
+
 public class User implements Serializable {
-    String email;
-    String password;
+    private String email, password;
+    private float hourlyWage, overtime, credits;
+    private Date startDate, endDate;
 
     public User() {
     }
